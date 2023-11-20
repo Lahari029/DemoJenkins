fadd502fafe10c7399d0e1558b3b9f3e2502615a
lokeshnagineni98 <lokeshnagineni98@gmail.com>
2023-11-21 00:30:55 +0530

    FirstCommit

---
+++ JenkinsDemo/src/test/java/TestCases/Tc2Test.java
@@ -0,0 +1,19 @@
+package TestCases;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Tc2Test {
+	@Test
+	public void tc2()
+	{
+		Reporter.log("Tc2 started",true);
+		
+		
+		
+	}
+
+
+}
